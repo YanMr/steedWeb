ee277e3eaf27505820f35bc3e425b5cde4ca473e
yanming5 <yanming5@xiaomi.com>
2021-02-20 16:40:36 +0800

    feat(): 添加场景任务弹窗

--- src/views/sceneManager/sceneLeft/Index.js
+++ src/views/sceneManager/sceneLeft/Index.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
-import { Input, Popover } from 'antd';
+import { Input, Popover, Modal, Form, Select, Radio, TimePicker, DatePicker } from 'antd';
 import IconFont from '@/components/IconFont';
+import 'moment/locale/zh-cn';
+import locale from 'antd/es/date-picker/locale/zh_CN';
 import '../index.scss'
 
+const { Option } = Select;
+const FormItem = Form.Item;
+
 class SceneLeft extends Component {
+  formRef = React.createRef();
   constructor(props) {
     super(props);
     this.state = {
       newScene: true,
       serch: false,
       sceneIndex: -1,
+      isModalVisible: true,
       sceneData: [
         {status: 1, name: '日常授课场景'},
         {status: 0, name: '期末考试'},
@@ -42,6 +49,20 @@ class SceneLeft extends Component {
     this.setState({
       sceneIndex: index
     })
+  }
+
+  // 添加场景弹窗
+  handleOk = () => {
+    
+  }
+
+  handleCancel = () => {
+
+  }
+
+  //  开机时间
+  onChange = (time, timeString) => {
+    console.log(time, timeString);
   }
 
   render() {
@@ -112,6 +133,104 @@ class SceneLeft extends Component {
           </div>
         {/* 场景详情 end */}
         </div>
+        {/* 添加场景弹窗 begin */}
+        <Modal title="场景添加"  okText="确定" cancelText="取消" visible={this.state.isModalVisible} onOk={() => this.handleOk()} onCancel={() => this.handleCancel()}>
+            <Form ref={this.formRef} labelAlign="left" className="login-form">
+                <FormItem 
+                  label="场景名称"
+									name="scenename"
+									rules={[{ required: true, message: '请填写场景名称!' }]}
+								>
+									<Input placeholder="任务名称" />
+								</FormItem>
+
+                <FormItem 
+                  label="场景状态"
+									name="scenestate"
+									rules={[{ required: true, message: '请选择场景状态!' }]}
+								>
+									<Radio.Group>
+                    <Radio value="1">启用</Radio>
+                    <Radio value="2">禁用</Radio>
+                  </Radio.Group>
+								</FormItem>
+
+                <FormItem 
+                  label="场景重复"
+									name="scenerepeat"
+									rules={[{ required: true, message: '请选择场景重复模式!' }]}
+								>
+									<Radio.Group>
+                    <Radio value="1">按周重复</Radio>
+                    <Radio value="2">指定日期</Radio>
+                  </Radio.Group>
+								</FormItem>
+
+                <FormItem 
+                  label="场景周期"
+									name="scenecycle"
+									rules={[{ required: true, message: '请选场景周期!' }]}
+								>
+									<Select
+                    mode="multiple"
+                    allowClear
+                    style={{ width: '100%' }}
+                    placeholder="请选择周期"
+                  >
+                    <Option key='0'>星期一</Option>
+                    <Option key='1'>星期二</Option>
+                    <Option key='2'>星期三</Option>
+                    <Option key='3'>星期四</Option>
+                    <Option key='4'>星期五</Option>
+                    <Option key='5'>星期六</Option>
+                    <Option key='6'>星期日</Option>
+                  </Select>
+								</FormItem>
+
+                <FormItem 
+                  label="开始时间"
+									name="sceneStarDate"
+									rules={[{ required: true, message: '请选场景开始时间!' }]}
+								>
+									<DatePicker style={{ width: '100%' }} locale={locale} placeholder="请选场景开始时间"/>
+								</FormItem>
+
+                <FormItem 
+                  label="结束时间"
+									name="sceneEndDate"
+									rules={[{ required: true, message: '请选场景结束时间!' }]}
+								>
+									<DatePicker style={{ width: '100%' }} locale={locale} placeholder="请选场景结束时间" />
+								</FormItem>
+
+                <FormItem 
+                  label="开机时间"
+									name="sceneboot"
+                  rules={[{ required: true, message: '请选场景开机时间!' }]}
+								>
+									<TimePicker style={{ width: '100%' }} locale={locale} onChange={this.onChange}  placeholder="请选开机时间"/>
+								</FormItem>
+
+                <FormItem 
+                  label="场景位置"
+									name="scenelocation"
+									rules={[{ required: true, message: '请选场景位置!' }]}
+								>
+								<Select
+                    allowClear
+                    style={{ width: '100%' }}
+                    placeholder="请选场景位置"
+                  >
+                    <Option key='0'>教学楼</Option>
+                    <Option key='1'>办公室</Option>
+                    <Option key='2'>自习楼</Option>
+                    <Option key='3'>舞蹈室</Option>
+                  </Select>
+								</FormItem>
+							
+							</Form>
+        </Modal>
+        {/* 添加场景弹窗 end */}
       </div>
     );
   }
