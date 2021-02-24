de63a1cf2eb8b74fd0944de397927526bb064b9b
yanming5 <yanming5@xiaomi.com>
2021-02-24 14:17:25 +0800

    feat(): 完善新建任务，并开发多选日历组件

--- src/views/sceneManager/newTask/Index.js
+++ src/views/sceneManager/newTask/Index.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-import { Input, Switch, Select, Radio, Button } from 'antd';
+import { Input, Switch, Select, Radio, Button, TimePicker } from 'antd';
 import BreadCrumb from '@/views/layout/BreadCrumb';
+import CheckCalendar from '@/components/CheckCalendar/Index'
 import IconFont from '@/components/IconFont';
+import moment from 'moment';
+import locale from 'antd/es/date-picker/locale/zh_CN';
 import '../index.scss'
 
 
@@ -10,7 +13,18 @@ class NewTask extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      radioValue: 1
+      radioValue: 1,
+      showCalendar: false,
+      weekDate: [{ state: '0', name: '周一', cur: false},
+        { state: '1', name: '周二', cur: false},
+        { state: '2', name: '周三', cur: false},
+        { state: '3', name: '周四', cur: false},
+        { state: '4', name: '周五', cur: false},
+        { state: '5', name: '周六', cur: false},
+        { state: '6', name: '周日', cur: false},
+      ],
+      isCheck: [],
+      palyList: ['']
     }
   }
 
@@ -18,7 +32,49 @@ class NewTask extends Component {
     this.setState({
       radioValue: e.target.value
     })
-    console.log(e.target.value)
+    if (e.target.value === 2) {
+      this.setState({
+        showCalendar: true
+      })
+    }
+  }
+
+  weekFun = index => {
+    const week = this.state.weekDate
+    week.map((item, num) => {
+      if (num === index) {
+        week[num].cur = true
+      } else {
+        week[num].cur = false
+      }
+    })
+    this.setState({
+      weekDate: week
+    })
+  }
+
+  showCalendarFun = () => {
+    this.setState({
+      showCalendar: true
+    })
+  }
+
+  //  添加播放时间
+  addPlay = () => {
+    const palyList = this.state.palyList;
+    palyList.push('')
+    this.setState({
+      palyList
+    })
+  }
+
+  // 删除播放时间
+  delPlay = index => {
+    const palyList = this.state.palyList;
+    palyList.splice(index, 1)
+    this.setState({
+      palyList
+    })
   }
 
   render() {
@@ -134,11 +190,74 @@ class NewTask extends Component {
               </Radio.Group>
               </div>
             </div>
+
+            {/* 按周重复begin */}
+            {this.state.radioValue === 1 ? (<div className="task-item">
+              <div className="task-cf-label">执行星期</div>
+              <div className="task-cf-value">
+                {
+                  this.state.weekDate.map((item, index) => {
+                    return (
+                      <div className="task-cf-value-item" key={index} className={item.cur ?'task-cf-value-item cur': 'task-cf-value-item'} onClick={() => this.weekFun(index)}>{item.name}</div>
+                    )
+                  })
+                }
+              </div>
+            </div>) : '' }
+            {/* 按周重复 end */}
+            {/* 指定日期 begin */}
+            {this.state.radioValue === 2 ? (<div className="task-item">
+              <div className="task-cf-label">执行日期</div>
+              <div className="task-cf-value">
+                {
+                  this.state.isCheck.map((item, index) => {
+                    return (
+                      <div className="task-cf-value-item" key={index}>{item}</div>
+                    )
+                  })
+                }
+                <IconFont type="icon-tianjia1" onClick={() => {this.showCalendarFun()}}/>
+              </div>
+            </div>) : ''}
+            {/* 指定日期 end */}
+
+            {/* 播放时间 begin */}
+            {
+              this.state.radioValue !==3 && this.state.palyList.map((itme, index) => {
+                return( <div className="task-item" key={index}>
+                <div className="task-cf-label">播放时间{index + 1}</div>
+                <div className="task-bf-value">
+                <TimePicker  style={{width: '220px'}} locale={locale}  placeholder="请选择播放时间"/>
+                {
+                  index === 0 ? (<IconFont type="icon-tianjia1" onClick={() => this.addPlay()} />) :
+                  (<IconFont type="icon-jianshao" className="del" onClick={() => this.delPlay(index)} />)
+                }
+                </div>
+              </div>)
+              })
+            }
+           
+            {/* 播放时间end */}
+
           </div>
 
           <div className="btn-task">
           <Button type="primary">确认提交</Button>
           </div>
+          <CheckCalendar
+          visible={this.state.showCalendar}
+          onCancel={()=>{
+                this.setState({
+                    showCalendar:false
+                })
+          }}
+          onConfirm={(isCheck)=>{
+                this.setState({
+                    isCheck,
+                    showCalendar:false
+                })
+          }}
+       />
 
         </div>
       </div>
