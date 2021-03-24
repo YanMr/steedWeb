2f884bc1c6cc7348ce775659b5e1edf24a5d5126
yanming5 <yanming5@xiaomi.com>
2021-03-24 22:19:07 +0800

    feat(): 任务详情

--- src/views/sceneManager/sceneRight/Index.js
+++ src/views/sceneManager/sceneRight/Index.js
@@ -1,196 +1,167 @@
 import React, { Component } from 'react';
 import IconFont from '@/components/IconFont';
+import iconFun from '@/components/IconType';
+import { getTaskDetails } from '@/server/scene'
+import _ from 'lodash'
 import '../index.scss'
 
 class SceneRight extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      task_property: {}
     }
+  }
+
+  // 获取任务详情
+  getTaskDetailsFun = async (taskId) => {
+    const { task_property } = await getTaskDetails({
+      task_id: taskId
+    })
+    this.setState({
+      task_property
+    })
+  }
+
+  weekFun = (week) => {
+    let text = ''
+     if (week == 1) {
+      text = "星期一";
+     }
+     if (week == 2) {
+      text = "星期二";
+     }
+     if (week == 3) {
+      text = "星期三";
+     }
+     if (week == 4) {
+      text = "星期四";
+     }
+     if (week == 5) {
+      text = "星期五";
+     }
+     if (week == 6) {
+      text = "星期六";
+     }
+     if (week == 7) {
+      text = "星期日";
+     }
+    return text
+  }
+
+  // 控制项图标获取
+  getIconFont = (type) => {
+    return iconFun(type)
   }
 
   render() {
     return (
-      <div className="serch-container">
-       <div className="serch-right-header">高二级开灯</div>
-       <div className="serch-right-container">
-         {/* 设备控制 begin */}
-         <div className="serch-right-device">
-           <div className="device-header">设备控制</div>
-           <div className="device-list">
-             <div className="device-item yes">
-               <div className="device-item-img"><IconFont type="icon-chuanglian-shouye" /></div>
-               <div className="device-item-name ellipsis">窗帘-开</div>
-             </div>
-             <div className="device-item yes">
-               <div className="device-item-img"><IconFont type="icon-diandeng-shouye" /></div>
-               <div className="device-item-name ellipsis">电灯-开</div>
-             </div>
-             <div className="device-item no">
-               <div className="device-item-img"><IconFont type="icon-fengshan-shouye" /></div>
-               <div className="device-item-name ellipsis">风扇-关</div>
-             </div>
-             <div className="device-item no">
-               <div className="device-item-img"><IconFont type="icon-xiaochengxutubiao-19" /></div>
-               <div className="device-item-name ellipsis">显示器-关</div>
-             </div>
-             <div className="device-item yes">
-               <div className="device-item-img"><IconFont type="icon-menjin-shouye" /></div>
-               <div className="device-item-name ellipsis">门禁-开</div>
-             </div>
-             <div className="device-item yes">
-               <div className="device-item-img"><IconFont type="icon-chuanglian-shouye" /></div>
-               <div className="device-item-name ellipsis">窗帘-开</div>
-             </div>
-             <div className="device-item yes">
-               <div className="device-item-img"><IconFont type="icon-diandeng-shouye" /></div>
-               <div className="device-item-name ellipsis">电灯-开</div>
-             </div>
-             <div className="device-item no">
-               <div className="device-item-img"><IconFont type="icon-fengshan-shouye" /></div>
-               <div className="device-item-name ellipsis">风扇-关</div>
-             </div>
-             <div className="device-item no">
-               <div className="device-item-img"><IconFont type="icon-xiaochengxutubiao-19" /></div>
-               <div className="device-item-name ellipsis">显示器-关</div>
-             </div>
-             <div className="device-item yes">
-               <div className="device-item-img"><IconFont type="icon-menjin-shouye" /></div>
-               <div className="device-item-name ellipsis">门禁-开</div>
-             </div>
-             
-           </div>
-         </div>
-         {/* 设备控制 end */}
-         {/* 接收成员 begin */}
-         <div className="device-user">
-         <div className="user-header">
-           <div className="user-header-title">接收成员</div>
-           <div className="user-header-ico"><IconFont type="icon-jinru" /></div>
-         </div>
-         <div className="user-container">
-           <div className="user-list">
-             <div className="user-list-header">分区1</div>
-             <div className="user-list-con">
-               <div className="user-list-item">高二(2)班</div>
-               <div className="user-list-item">高二(2)班</div>
-               <div className="user-list-item">高二(2)班</div>
-               <div className="user-list-item">高二(2)班</div>
-             </div>
-           </div>
+      <>
+      {
+        _.isEqual(this.state.task_property, {}) ? (<div className="noData">请选择任务</div>) : ( <div className="serch-container">
+        <div className="serch-right-header">{this.state.task_property.name}</div>
+        <div className="serch-right-container">
+          {/* 设备控制 begin */}
+          <div className="serch-right-device">
+            <div className="device-header">设备控制</div>
+            <div className="device-list">
+              {
+                this.state.task_property?.iot_control?.map(item => {
+                  return (
+                   <div className={item.state? 'device-item yes' : 'device-item no'}>
+                     <div className="device-item-img"><IconFont type={this.getIconFont()} /></div>
+                     <div className="device-item-name ellipsis">{item.name}-{item.state?'开':'关'}</div>
+                   </div>
+                  )
+                })
+              }
+              
+              
+            </div>
+          </div>
+          {/* 设备控制 end */}
+          {/* 接收成员 begin */}
+          <div className="device-user">
+          <div className="user-header">
+            <div className="user-header-title">接收成员</div>
+            <div className="user-header-ico"><IconFont type="icon-jinru" /></div>
+          </div>
+          <div className="user-container">
+            {
+              this.state.task_property?.placelist?.map(itme =>  <div className="user-list">
+              <div className="user-list-header">{itme.name}</div>
+              <div className="user-list-con">
+                {
+                  itme.room?.map(falg => <div className="user-list-item">{falg.name}</div>)
+                }
+              </div>
+            </div>)
+            }
            
-           <div className="user-list">
-             <div className="user-list-header">分区1</div>
-             <div className="user-list-con">
-               <div className="user-list-item">高二(2)班</div>
-               <div className="user-list-item">高二(2)班</div>
-               <div className="user-list-item">高二(2)班</div>
-               <div className="user-list-item">高二(2)班</div>
-             </div>
-           </div>
-
-           <div className="user-list">
-             <div className="user-list-header">分区1</div>
-             <div className="user-list-con">
-               <div className="user-list-item">高二(2)班</div>
-               <div className="user-list-item">高二(2)班</div>
-               <div className="user-list-item">高二(2)班</div>
-               <div className="user-list-item">高二(2)班</div>
-             </div>
-           </div>
-
-           <div className="user-list">
-             <div className="user-list-header">分区1</div>
-             <div className="user-list-con">
-               <div className="user-list-item">高二(2)班</div>
-               <div className="user-list-item">高二(2)班</div>
-               <div className="user-list-item">高二(2)班</div>
-               <div className="user-list-item">高二(2)班</div>
-             </div>
-           </div>
-
-         </div>
-
-          <div className="user-footer">
-            共45个
+            
           </div>
-
-         </div>
-         {/* 接收成员 end */}
-
-         {/* 生效时间 begin */}
-         <div className="device-user">
-         <div className="user-header">
-           <div className="user-header-title">接收成员</div>
-           <div className="user-header-ico"><IconFont type="icon-jinru" /></div>
-         </div>
-         <div className="user-container">
-           <div className="effect-date">
-           <div className="user-container-title">执行时间</div>
-           <div className="user-container-list">
-             <div className="user-date-item">08:30:00</div>
-             <div className="user-date-item">08:30:00</div>
-             <div className="user-date-item">08:30:00</div>
-             <div className="user-date-item">08:30:00</div>
-             <div className="user-date-item">08:30:00</div>
-             <div className="user-date-item">08:30:00</div>
-             <div className="user-date-item">08:30:00</div>
-             <div className="user-date-item">08:30:00</div>
-             <div className="user-date-item">08:30:00</div>
-             <div className="user-date-item">08:30:00</div>
-             <div className="user-date-item">08:30:00</div>
-             <div className="user-date-item">08:30:00</div>
-             <div className="user-date-item">08:30:00</div>
-             <div className="user-date-item">08:30:00</div>
-             <div className="user-date-item">08:30:00</div>
+ 
+           <div className="user-footer">
+             共{this.state.task_property?.place_id?.length > 0 ? this.state.task_property?.place_id.length : 0}个
            </div>
-           </div>
-         </div>
-
-          <div className="user-footer">
-            共45个
+ 
           </div>
-
-         </div>
-         {/* 生效时间 end */}
-
-         {/* 重复 begin */}
-         <div className="device-user">
-         <div className="user-header">
-           <div className="user-header-title">重复</div>
-           <div className="user-header-ico"><IconFont type="icon-jinru" /></div>
-         </div>
-         <div className="user-container">
-           <div className="effect-date">
-           <div className="user-container-title">执行时间</div>
-           <div className="user-container-list">
-             <div className="user-date-item">08:30:00</div>
-             <div className="user-date-item">08:30:00</div>
-             <div className="user-date-item">08:30:00</div>
-             <div className="user-date-item">08:30:00</div>
-             <div className="user-date-item">08:30:00</div>
-             <div className="user-date-item">08:30:00</div>
-             <div className="user-date-item">08:30:00</div>
-             <div className="user-date-item">08:30:00</div>
-             <div className="user-date-item">08:30:00</div>
-             <div className="user-date-item">08:30:00</div>
-             <div className="user-date-item">08:30:00</div>
-             <div className="user-date-item">08:30:00</div>
-             <div className="user-date-item">08:30:00</div>
-             <div className="user-date-item">08:30:00</div>
-             <div className="user-date-item">08:30:00</div>
-           </div>
-           </div>
-         </div>
-
-          <div className="user-footer">
-            共45个
+          {/* 接收成员 end */}
+ 
+          {/* 生效时间 begin */}
+          <div className="device-user">
+          <div className="user-header">
+            <div className="user-header-title">{this.state.task_property.time_type === 1 ? '按周重复' : '按日期重复'}</div>
+            <div className="user-header-ico"><IconFont type="icon-jinru" /></div>
           </div>
-
-         </div>
-         {/* 重复 end */}
-       </div>
-      </div>
+          <div className="user-container">
+            <div className="effect-date">
+            <div className="user-container-title">{this.state.task_property.time_type === 1 ? '指定星期' : '指定日期'}</div>
+            <div className="user-container-list">
+              {
+               this.state.task_property.time_type === 1 ? (<>{
+                 this.state.task_property?.time?.week?.map(item => <div className="user-date-item">{this.weekFun(item)}</div>)
+               }</>) : (<>{
+                 this.state.task_property?.time?.date?.map(item => <div className="user-date-item">{item}</div>)
+               }</>)
+              }
+              
+            </div>
+            </div>
+          </div>
+ 
+           <div className="user-footer">
+             共{this.state.task_property.time_type === 1 ? this.state.task_property?.time?.week.length : this.state.task_property?.date?.week.length}个
+           </div>
+ 
+          </div>
+          {/* 生效时间 end */}
+ 
+          {/* 重复 begin */}
+          <div className="device-user">
+          <div className="user-header">
+            <div className="user-header-title">播放时间</div>
+            <div className="user-header-ico"><IconFont type="icon-jinru" /></div>
+          </div>
+          <div className="user-container">
+            <div className="effect-date">
+            <div className="user-container-title">执行时间</div>
+            <div className="user-container-list">
+             {this.state.task_property?.time?.time?.map(item => <div className="user-date-item">{item}</div>)}
+            </div>
+            </div>
+          </div>
+ 
+           <div className="user-footer">
+             共{this.state.task_property?.time?.time.length}个
+           </div>
+ 
+          </div>
+          {/* 重复 end */}
+        </div>
+       </div>)
+      }
+      </>
     );
   }
 }
